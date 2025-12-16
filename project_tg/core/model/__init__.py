043a23be5a155e40e139966925fda29c47cd603c
harmless95 <213446016+harmless95@users.noreply.github.com>
2025-12-16 18:26:35 +0700

    create model view

--- project_tg/core/model/__init__.py
+++ project_tg/core/model/__init__.py
@@ -1,7 +1,6 @@
-__all__ = (
-    "BaseDB",
-    "db_helper_conn",
-)
+__all__ = ("BaseDB", "db_helper_conn", "Video", "VideoSnapshots")
 
 from Base import BaseDB
 from help_db import db_helper_conn
+from videos import Video
+from video_snapshots import VideoSnapshots
