ce603e008538af1fbce506f142912d856038c6bf
harmless95 <213446016+harmless95@users.noreply.github.com>
2025-12-16 18:48:50 +0700

    create main router

---
+++ project_tg/main.py
@@ -0,0 +1,24 @@
+import asyncio
+
+from aiogram import Bot, Dispatcher
+from aiogram.enums.parse_mode import ParseMode
+from aiogram.client.default import DefaultBotProperties
+from aiogram.fsm.storage.memory import MemoryStorage
+
+from core.config import setting
+
+async def main() -> None:
+
+    bot = Bot(
+        token=setting.t_bot.token,
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML),
+    )
+    dp = Dispatcher(storage=MemoryStorage())
+
+    await bot.delete_webhook(drop_pending_updates=True)
+
+    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
