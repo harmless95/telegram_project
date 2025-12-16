0d63b0b7f2e46630595a54d2a6768d271d547bd8
harmless95 <213446016+harmless95@users.noreply.github.com>
2025-12-16 18:26:10 +0700

    create relationship

--- project_tg/core/model/videos.py
+++ project_tg/core/model/videos.py
@@ -1,10 +1,14 @@
+from typing import TYPE_CHECKING
 from uuid import UUID
 from datetime import datetime
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import TIMESTAMP, Integer, func
 
 from core.model import BaseDB
 from core.model.mixins import IdPrKey
+
+if TYPE_CHECKING:
+    from .video_snapshots import VideoSnapshots
 
 
 class Video(BaseDB, IdPrKey):
@@ -16,3 +20,7 @@ class Video(BaseDB, IdPrKey):
     likes_count: Mapped[int] = mapped_column(Integer, default=0, nullable=False)
     comments_count: Mapped[int] = mapped_column(Integer, default=0, nullable=False)
     reports_count: Mapped[int] = mapped_column(Integer, default=0, nullable=False)
+    snapshots: Mapped[list["VideoSnapshots"]] = relationship(
+        back_populates="video",
+        cascade="all, delete-orphan",
+    )
