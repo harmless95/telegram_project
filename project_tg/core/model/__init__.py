2d364c3e40b63097162df7375ad41a7003a330b1
harmless95 <213446016+harmless95@users.noreply.github.com>
2025-12-17 16:43:43 +0700

    corrected name table

--- project_tg/core/model/__init__.py
+++ project_tg/core/model/__init__.py
@@ -1,6 +1,6 @@
-__all__ = ("BaseDB", "db_helper_conn", "Video", "VideoSnapshots")
+__all__ = ("BaseDB", "db_helper_conn", "Video", "VideoSnapshot")
 
 from .Base import BaseDB
 from .help_db import db_helper_conn
 from .videos import Video
-from .video_snapshots import VideoSnapshots
+from .video_snapshots import VideoSnapshot
