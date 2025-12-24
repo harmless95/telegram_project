1c1b1b3b5b8660d91612c68a24d534cf9ee9d7d4
harmless95 <213446016+harmless95@users.noreply.github.com>
2025-12-24 16:47:13 +0700

    corrected sync client to AsyncOpenAI client

--- project_tg/api/Dependencies/generate_sql_query.py
+++ project_tg/api/Dependencies/generate_sql_query.py
@@ -3,15 +3,13 @@ from api.Dependencies.llm import SQL_PROMPT
 
 async def generate_sql(query: str, client) -> str:
     prompt = SQL_PROMPT.format(query=query)
-
+    message = [{"role": "user", "content": prompt}]
+    model = "llama-3.1-8b-instant"
     try:
-        response = client.chat.completions.create(
-            messages=[{"role": "user", "content": prompt}],
-            model="llama-3.1-8b-instant",
-        )
+        response = await client.responses.create(model=model, input=message)
     except Exception as ex:
         return ""
 
-    sql = response.choices[0].message.content.strip()
+    sql = response.output_text
 
     return sql
